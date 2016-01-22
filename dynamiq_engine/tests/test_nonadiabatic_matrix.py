6a68482d7b96d1801ff2e47c527189d9c3678470
David W.H. Swenson <dwhs@hyperblazer.net>
2016-01-21 21:45:00 -0500

    Add skeleton of test_nonadiabatic_matrix

---
+++ dynamiq_engine/tests/test_nonadiabatic_matrix.py
@@ -0,0 +1,33 @@
+import dynamiq_engine as dynq
+import numpy as np
+from tools import *
+import stubs
+from dynamiq_engine.nonadiabatic_matrix import *
+
+class testNonadiabaticMatrix(object):
+    def setup(self):
+        pass
+
+    def check_build_from_number_matrix(self):
+        raise SkipTest
+
+    def check_build_from_mixed_matrix(self):
+        raise SkipTest
+
+    def check_build_from_number_dictionary(self):
+        raise SkipTest
+
+    def check_build_from_mixed_dictionary(self):
+        raise SkipTest
+
+    def check_numeric_matrix_numeric_input(self):
+        raise SkipTest
+
+    def check_numeric_matrix_fucntion_input(self):
+        raise SkipTest
+
+    def check_get_item(self):
+        raise SkipTest
+
+    def bad_entries_string(self):
+        raise SkipTest
