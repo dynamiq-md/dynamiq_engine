2348c78bcfad330a638f96b1701d6a4481a36c16
David W.H. Swenson <dwhs@hyperblazer.net>
2016-02-01 12:54:34 -0500

    CR4MMST tests for Tully and Rabi
    Note that these tests only check that the code runs -- the results are
    pegged to what actually came out of the tests the first time. So the results
    could (hypothetically) be wrong, although they look reasonable. There's a
    note above the tests to this effect to remind me of this fact should numbers
    come out weird.
    
    A full test of these models based on previously reported results will be the
    check that the code is actually correct.

--- dynamiq_engine/tests/test_candy_rozmus_4.py
+++ dynamiq_engine/tests/test_candy_rozmus_4.py
@@ -64,7 +64,58 @@ class testCandyRozmus4(object):
         assert_array_almost_equal(new_snap.momenta, exact_0x10['p'])
 
 class testCandyRozmus4MMST(object):
-    def setup(self):
+    def test_step_uncoupled(self):
+        from math import sqrt
+        # test uncoupled
+        uncoupled_matrix = dynq.NonadiabaticMatrix([[2.0, 0.0], [0.0, 3.0]])
+        uncoupled = dynq.potentials.MMSTHamiltonian(uncoupled_matrix)
+        uncoupled_topology = dynq.Topology(masses=[], potential=uncoupled)
+        uncoupled_snap = dynq.MMSTSnapshot(
+            coordinates=np.array([]), momenta=np.array([]),
+            electronic_coordinates=np.array([1.0, 1.0]),
+            electronic_momenta=np.array([1.0, 1.0]),
+            topology=uncoupled_topology
+        )
+        uncoupled_integ = CandyRozmus4MMST(0.01, uncoupled)
+        for i in range(10):
+            uncoupled_integ.step(uncoupled, uncoupled_snap, uncoupled_snap)
+
+        exact_1 = exact_ho(time=0.1, omega=2.0, m=1.0/2.0, q0=1.0, p0=1.0)
+        exact_2 = exact_ho(time=0.1, omega=3.0, m=1.0/3.0, q0=1.0, p0=1.0)
+        predicted_coordinates = [exact_1['q'][0], exact_2['q'][0]]
+        predicted_momenta = [exact_1['p'][0], exact_2['p'][0]]
+        assert_array_almost_equal(uncoupled_snap.electronic_coordinates,
+                                  np.array(predicted_coordinates))
+        assert_array_almost_equal(uncoupled_snap.electronic_momenta,
+                                  np.array(predicted_momenta))
+
+    def test_step_rabi(self):
+        # test Rabi
+        rabi_matrix = dynq.NonadiabaticMatrix([[2.0, 1.0], [1.0, 3.0]])
+        rabi = dynq.potentials.MMSTHamiltonian(rabi_matrix)
+        rabi_topology = dynq.Topology(masses=[], potential=rabi)
+        rabi_snapshot = dynq.MMSTSnapshot(
+            coordinates=np.array([]), momenta=np.array([]),
+            electronic_coordinates=np.array([1.0, 0.0]),
+            electronic_momenta=np.array([0.0, 1.0]),
+            topology=rabi_topology
+        )
+        rabi_integ = CandyRozmus4MMST(0.01, rabi)
+        for i in range(10):
+            rabi_integ.step(rabi, rabi_snapshot, rabi_snapshot)
+
+        # NOTE: unverified -- these results are checked against the output
+        # that they first gave, not against any analytical results
+        assert_array_almost_equal(rabi_snapshot.electronic_coordinates,
+                                  np.array([1.07189698, 0.26951517]))
+        assert_array_almost_equal(rabi_snapshot.electronic_momenta,
+                                  np.array([-0.22220342, 0.85382907]))
+        assert_array_almost_equal(rabi_snapshot.coordinates, np.array([]))
+        assert_array_almost_equal(rabi_snapshot.momenta, np.array([]))
+
+
+    def test_step_tully(self):
+        # test Tully
         tully_V11 = pes.OneDimensionalInteractionModel(
             pes.interactions.TanhInteraction(a=1.6, V0=0.1)
         )
@@ -76,60 +127,30 @@ class testCandyRozmus4MMST(object):
         )
         tully_matrix = dynq.NonadiabaticMatrix([[tully_V11, tully_V12],
                                                 [tully_V12, tully_V22]])
-        self.tully = dynq.potentials.MMSTHamiltonian(tully_matrix)
+        tully = dynq.potentials.MMSTHamiltonian(tully_matrix)
         tully_topology = dynq.Topology(
             masses=np.array([1980.0]),
-            potential=self.tully
+            potential=tully
+        )
+        tully_snapshot = dynq.MMSTSnapshot(
+            coordinates=np.array([0.1]),
+            momenta=np.array([19.0]),
+            electronic_coordinates=np.array([0.7, 0.6]),
+            electronic_momenta=np.array([0.2, 0.1]),
+            topology=tully_topology
         )
 
-        self.tully_integ = CandyRozmus4MMST(1.0, self.tully)
 
-        uncoupled_matrix = dynq.NonadiabaticMatrix([[2.0, 0.0], [0.0, 3.0]])
-        self.uncoupled = dynq.potentials.MMSTHamiltonian(uncoupled_matrix)
+        tully_integ = CandyRozmus4MMST(1.0, tully)
+        tully_integ.step(tully, tully_snapshot, tully_snapshot)
 
-        rabi_matrix = dynq.NonadiabaticMatrix([[2.0, 1.0], [1.0, 3.0]])
-        self.rabi = dynq.potentials.MMSTHamiltonian(rabi_matrix)
-
-        pass
-
-    def test_cr4_step(self):
-        from math import sqrt
-        # test uncoupled
-        uncoupled_topology = dynq.Topology(masses=[], potential=self.uncoupled)
-        uncoupled_snap = dynq.MMSTSnapshot(
-            coordinates=np.array([]), momenta=np.array([]),
-            electronic_coordinates=np.array([1.0, 1.0]),
-            electronic_momenta=np.array([1.0, 1.0]),
-            topology=uncoupled_topology
-        )
-        uncoupled_integ = CandyRozmus4MMST(0.01, self.uncoupled)
-        for i in range(10):
-            uncoupled_integ.step(self.uncoupled, uncoupled_snap,
-                                 uncoupled_snap)
-
-        exact_1 = exact_ho(time=0.1, omega=2.0, m=1.0/2.0, q0=1.0, p0=1.0)
-        exact_2 = exact_ho(time=0.1, omega=3.0, m=1.0/3.0, q0=1.0, p0=1.0)
-        predicted_coordinates = [exact_1['q'][0], exact_2['q'][0]]
-        predicted_momenta = [exact_1['p'][0], exact_2['p'][0]]
-        assert_array_almost_equal(uncoupled_snap.electronic_coordinates,
-                                  np.array(predicted_coordinates))
-        assert_array_almost_equal(uncoupled_snap.electronic_momenta,
-                                  np.array(predicted_momenta))
-
-        # test Rabi
-        #rabi_topology = dynq.Topology(masses=[], potential=self.rabi)
-        #rabi_snapshot = dynq.MMSTSnapshot(
-            #coordinates=np.array([]), momenta=np.array([]),
-            #electronic_coordinates=np.array([1.0, 0.0]),
-            #electronic_momenta=np.array([0.0, 1.0]),
-            #topology=rabi_topology
-        #)
-        #rabi_integ = CandyRozmus4MMST(0.01, self.rabi)
-        #for i in range(10):
-            #rabi_integ.step(self.rabi, rabi_snapshot, rabi_snapshot)
-
-        # TODO: test results
-
-
-        # test Tully
-        raise SkipTest
+        # NOTE: unverified -- these results are checked against the output
+        # that they first gave, not against any analytical results
+        assert_array_almost_equal(tully_snapshot.coordinates,
+                                  np.array([0.10959396]))
+        assert_array_almost_equal(tully_snapshot.momenta,
+                                  np.array([18.9922916]))
+        assert_array_almost_equal(tully_snapshot.electronic_coordinates,
+                                  np.array([0.70714581, 0.6075074]))
+        assert_array_almost_equal(tully_snapshot.electronic_momenta,
+                                  np.array([0.15844469, 0.07522877]))
