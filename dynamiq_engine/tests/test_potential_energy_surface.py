610f5531a34b2ee6d971d605d472235ca56b9add
David W.H. Swenson <dwhs@hyperblazer.net>
2016-02-15 17:38:20 -0500

    Restructure of 1D PES tests

--- dynamiq_engine/tests/test_potential_energy_surface.py
+++ dynamiq_engine/tests/test_potential_energy_surface.py
@@ -8,62 +8,54 @@ from dynamiq_engine.potentials.potential_energy_surface import *
 class testOneDimensionalInteractionModel(object):
     def setup(self):
         self.ho = pes.interactions.HarmonicOscillatorInteraction(k=2.0, x0=1.0)
-        self.pot = OneDimensionalInteractionModel(self.ho)
-        topology = dynq.Topology(masses=np.array([0.5]), potential=self.pot)
-        self.test_snap1 = dynq.Snapshot(
-            coordinates=np.array([1.0]),
-            momenta=np.array([1.0]),
-            topology=topology
-        )
-        self.test_snap2 = dynq.Snapshot(
-            coordinates=np.array([0.5]),
-            momenta=np.array([2.0]),
-            topology=topology
-        )
-        self.test_snap3 = dynq.Snapshot(
-            coordinates=np.array([2.0]),
-            momenta=np.array([-0.5]),
-            topology=topology
-        )
+        self.ho_pot = OneDimensionalInteractionModel(self.ho)
+        ho_topol = dynq.Topology(masses=np.array([0.5]), potential=self.ho_pot)
+        self.ho_test_snaps = [
+            dynq.Snapshot(coordinates=np.array([1.0]),
+                          momenta=np.array([1.0]),
+                          topology=ho_topol),
+            dynq.Snapshot(coordinates=np.array([0.5]),
+                          momenta=np.array([2.0]),
+                          topology=ho_topol),
+            dynq.Snapshot(coordinates=np.array([2.0]),
+                          momenta=np.array([-0.5]),
+                          topology=ho_topol)
+        ]
+        self.morse = pes.interactions.MorseInteraction(30.0, 0.08, 0.5)
 
     def test_H(self):
-        assert_almost_equal(self.pot.H(self.test_snap1), 1.0)
-        assert_almost_equal(self.pot.H(self.test_snap2), 4.25)
-        assert_almost_equal(self.pot.H(self.test_snap3), 1.25)
+        tests = {
+            self.ho_test_snaps[0] : 1.0,
+            self.ho_test_snaps[1] : 4.25,
+            self.ho_test_snaps[2] : 1.25
+        }
+        check_function(self.ho_pot.H, tests)
 
     def test_T(self):
         # Definition of T is $L + V = p * dH/dp - H + V$; test this directly.
         # Use a lambda rather than nested def because nested def screws with
         # my in-editor test runner
         explicit_T = lambda pes, snap : (
-            np.dot(snap.momenta, pes.dHdp(snap))
-            - self.pot.H(snap) + self.pot.V(snap)
+            np.dot(snap.momenta, pes.dHdp(snap)) - pes.H(snap) + pes.V(snap)
         )
-
-        assert_almost_equal(self.pot.T(self.test_snap1),
-                            explicit_T(self.pot, self.test_snap1))
-        assert_almost_equal(self.pot.T(self.test_snap2),
-                            explicit_T(self.pot, self.test_snap2))
-        assert_almost_equal(self.pot.T(self.test_snap3),
-                            explicit_T(self.pot, self.test_snap3))
-
+        tests = {s : explicit_T(self.ho_pot, s) for s in self.ho_test_snaps}
+        check_function(self.ho_pot.T, tests)
 
     def test_dHdq(self):
-        assert_array_almost_equal(self.pot.dHdq(self.test_snap1), 
-                                  np.array([0.0]))
-        assert_array_almost_equal(self.pot.dHdq(self.test_snap2), 
-                                  np.array([-1.0]))
-        assert_array_almost_equal(self.pot.dHdq(self.test_snap3), 
-                                  np.array([2.0]))
+        tests = {
+            self.ho_test_snaps[0] : np.array([0.0]),
+            self.ho_test_snaps[1] : np.array([-1.0]),
+            self.ho_test_snaps[2] : np.array([2.0])
+        }
+        check_function(self.ho_pot.dHdq, tests)
 
     def test_dHdp(self):
-        assert_array_almost_equal(self.pot.dHdp(self.test_snap1),
-                                  np.array([2.0]))
-        assert_array_almost_equal(self.pot.dHdp(self.test_snap2),
-                                  np.array([4.0]))
-        assert_array_almost_equal(self.pot.dHdp(self.test_snap3),
-                                  np.array([-1.0]))
-
+        tests = {
+            self.ho_test_snaps[0] : np.array([2.0]),
+            self.ho_test_snaps[1] : np.array([4.0]),
+            self.ho_test_snaps[2] : np.array([-1.0])
+        }
+        check_function(self.ho_pot.dHdp, tests)
 
     def test_d2Hdq2(self):
         raise SkipTest
