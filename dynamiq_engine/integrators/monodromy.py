1fc84a51319fd34fb75f68006ca5e0cf21be59d0
David W.H. Swenson <dwhs@hyperblazer.net>
2016-02-15 03:36:38 -0500

    Skeleton of monodromy integrator-helper.

---
+++ dynamiq_engine/integrators/monodromy.py
@@ -0,0 +1,16 @@
+
+
+class MonodromyHelper(object):
+    def __init__(self, integrator):
+        pass
+
+    def prepare(self):
+        raise NotImplementedError
+
+class StandardMondromy(MonodromyHelper):
+    def __init__(self, integrator):
+        pass
+
+    def prepare(self):
+        pass
+
